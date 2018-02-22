81cc04bf5a7080dbf89de4fbdb317736f500c405
said <said.hmidi@esprit.tn>
2018-02-22 22:45:47 +0000

    Interfaces Membre (MembreView+ModifierProfile).

---
+++ src/GUI/membre/MembreViewController.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI.membre;
+
+import utile.Upload;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
+import services.UserDAO;
+import espacesante.bienetre.EspaceSanteBienEtre ;
+
+
+public class MembreViewController implements Initializable {
+
+    @FXML
+    private ImageView photo;
+    @FXML
+    private Pane chnager;
+    @FXML
+    private AnchorPane content;
+    File file;
+    @FXML
+    private Label currentUser;
+    @FXML
+    private ImageView newslater;
+    Upload up =new Upload();
+    UserDAO userDAO=new UserDAO();
+    @FXML
+    private ImageView mark;
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //mark.setVisible(false);
+
+        currentUser.setText(EspaceSanteBienEtre.currentUser.getFirstname()+"  "+EspaceSanteBienEtre.currentUser.getLastname());
+        chnager.setOnMouseEntered((MouseEvent event)->
+                {
+                    chnager.setVisible(true);
+                });
+         chnager.setOnMouseExited((MouseEvent event)->
+                {
+                    chnager.setVisible(false);
+                });
+         photo.setOnMouseEntered((MouseEvent event)->
+                {
+                    chnager.setVisible(true);
+                });
+         photo.setOnMouseExited((MouseEvent event)->
+                {
+                    chnager.setVisible(false);
+                });
+       
+    File file = new File("C:\\Users\\said hmidi\\Desktop\\Piedev\\EspaceSante&BienEtre\\image\\"+EspaceSanteBienEtre.currentUser.getAvatar());
+    Image image = new Image(file.toURI().toString());
+    photo.setImage(image);
+    
+    }    
+
+
+    @FXML
+    private void apropo(MouseEvent event) throws IOException {
+         Parent parent = FXMLLoader.load(getClass().getResource("/GUI/membre/modifier.fxml"));
+        parent.setStyle("-fx-box-border: transparent;");
+        content.getChildren().clear();
+        content.getChildren().add(parent);
+        AnchorPane.setTopAnchor(parent, 0d);
+        AnchorPane.setRightAnchor(parent, 0d);
+        AnchorPane.setBottomAnchor(parent, 0d);
+        AnchorPane.setLeftAnchor(parent, 0d);
+    }
+
+    @FXML
+    private void photochange(MouseEvent event) 
+    {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("*.jpg", "*.JPG", "*.png");
+        fileChooser.getExtensionFilters().addAll(extFilter);
+        file = fileChooser.showOpenDialog(null);
+
+        try {
+            BufferedImage bufferedImage = ImageIO.read(file);
+
+            WritableImage image = SwingFXUtils.toFXImage(bufferedImage, null);
+            photo.setImage(image);
+            up.upload(file);
+            userDAO.update_photo(file.getName(), EspaceSanteBienEtre.currentUser.getId());
+        } catch (IOException ex) {
+           
+        } 
+    }
+
+    
+/*
+    @FXML
+    private void newslater(MouseEvent event) {
+        newslater.setDisable(true);
+        mark.setVisible(true);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Newsletter!");
+        alert.setContentText("Votre inscription a notre newsletter et terminé avec success!");
+        alert.showAndWait();
+    }
+*/
+   
+    
+    
+    
+    @FXML
+    private void logout(MouseEvent event) {
+         EspaceSanteBienEtre.logout(); 
+    }
+    
+}
---
+++ src/GUI/membre/ModifierController.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI.membre;
+
+import com.jfoenix.controls.JFXTextField;
+import com.twilio.type.Client;
+import entites.User;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import services.UserDAO;
+import espacesante.bienetre.EspaceSanteBienEtre;
+import utile.Mail;
+
+
+public class ModifierController implements Initializable {
+
+    @FXML
+    private JFXTextField prenom;
+    @FXML
+    private JFXTextField mail;
+    @FXML
+    private JFXTextField tel;
+    @FXML
+    private JFXTextField password;
+    @FXML
+    private JFXTextField nom;
+    @FXML
+    private JFXTextField password2;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+         nom.setText(EspaceSanteBienEtre.currentUser.getFirstname());
+        prenom.setText(EspaceSanteBienEtre.currentUser.getLastname());
+        mail.setText(EspaceSanteBienEtre.currentUser.getEmail());
+        password.setText(EspaceSanteBienEtre.currentUser.getPassword());
+        password2.setText(EspaceSanteBienEtre.currentUser.getPassword());
+        tel.setText(EspaceSanteBienEtre.currentUser.getTelephone());
+        System.out.println(EspaceSanteBienEtre.currentUser.getAvatar());
+  
+    }    
+
+    @FXML
+    private void modifier(MouseEvent event) {
+         
+            User user = new User();
+            UserDAO userDAO=new UserDAO();
+            System.out.println(EspaceSanteBienEtre.currentUser.getRole());
+
+            user.setId(EspaceSanteBienEtre.currentUser.getId());
+            user.setUsername(EspaceSanteBienEtre.currentUser.getUsername());
+            user.setPassword(password.getText());
+            user.setEmail(mail.getText());
+            user.setFirstname(nom.getText());
+            user.setLastname(prenom.getText());
+            user.setAdress(EspaceSanteBienEtre.currentUser.getAdress());
+            user.setTelephone(tel.getText());
+            user.setStatus(EspaceSanteBienEtre.currentUser.getStatus());
+
+            userDAO.update(user);
+            System.out.println(user.getEmail());
+          
+                Mail.sendMail(user.getEmail(), "Compte Espace sante", "Votre compte en tant que"+user.getRole()+" chez Espace sante a été modifier avec succsée: " 
+                      +"\n votre nouveau statut est: "+ user.getStatus()
+                      +"\n votre nouveau adresse est: "  +user.getAdress()
+                      +"\n votre nouveau nom est: "  +user.getFirstname()
+                      +"\n votre nouveau prenom est: "  +user.getLastname()
+                      +"\n votre nouveau mot de passe est: " +user.getPassword()
+                      +"\n votre nouveau numero de telephone est: "  +user.getTelephone()
+                      );
+           
+
+           
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success!");
+            alert.setContentText("Utilisateur modifier avec success!");
+            alert.showAndWait();
+        }
+    
+    
+}
