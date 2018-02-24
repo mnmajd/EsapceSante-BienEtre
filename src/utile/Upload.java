444b532f3a1be6973934189472480bd67b731ed8
said <said.hmidi@esprit.tn>
2018-02-24 11:04:08 +0000

    Utile:  Upload+UserStatus.

---
+++ src/utile/Upload.java
@@ -0,0 +1,53 @@
+package utile;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Upload {
+    
+    public String upload(File file) throws FileNotFoundException, IOException {
+        BufferedOutputStream stream = null;
+        String globalPath="image";
+        String localPath="localhost:8888/";
+        String fileName = file.getName();
+        fileName=fileName.replace(" ", "_");
+        try {
+            Path p = file.toPath();
+            
+            byte[] bytes = Files.readAllBytes(p);
+    
+            File dir = new File(globalPath);
+            if (!dir.exists())
+                dir.mkdirs();
+            // Create the file on server
+            File serverFile = new File(dir.getAbsolutePath()+File.separator + fileName);
+            stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+            return localPath+"/"+fileName;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Upload.class.getName()).log(Level.SEVERE, null, ex);
+            return "error1";
+        } catch (IOException ex) {
+            Logger.getLogger(Upload.class.getName()).log(Level.SEVERE, null, ex);
+            return "error2";
+        }
+    }
+}
---
+++ src/utile/UserStatus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utile;
+
+
+public class UserStatus {
+     public final static String PENDING = "pending";
+    public final static String REFUSED = "refused";
+    public final static String ACTIVE  = "active";
+    public final static String BLOCKED = "blocked";
+    public final static String DELETED = "deleted";
+    
+}
